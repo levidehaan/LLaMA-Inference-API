24a935adc81db33c46a74149ab18b7896a3f5896
Levi DeHaan <levi@levidehaan.com>
2023-04-17 05:46:20 +0000

    removing junk

--- setup.sh
+++ setup.sh
@@ -6,8 +6,5 @@ python setup.py
 pip install git+https://github.com/aniketmaurya/llama-inference-api.git@main
 #manually install lit-llama
 pip install lit-llama@git+https://github.com/Lightning-AI/lit-llama.git@main
-#install zsh and oh-my-zsh
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#install azure cli
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+echo "thank you, please come again"
