4fdc1b226e4b983b5ac1773a209c246deb851da9
Levi DeHaan <levi@levidehaan.com>
2023-04-17 05:55:23 +0000

    adding gradient

---
+++ .gradient/prepare-datasets.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash 
+set -uxo pipefail
+
+if [ ! "$(command -v fuse-overlayfs)" ]
+then
+    echo "fuse-overlayfs not found installing - please update to our latest image"
+    apt update -y
+    apt install -o DPkg::Lock::Timeout=120 -y psmisc libfuse3-dev fuse-overlayfs
+fi
+
+
+echo "Starting preparation of datasets"
+/notebooks/.gradient/symlink_datasets_and_caches.py
+
+echo "Finished running setup.sh."
+# Run automated test if specified
+if [[ "${1:-}" == 'test' ]]; then
+    /notebooks/.gradient/automated-test.sh "${@:2}"
+elif [[ "${2:-}" == 'test' ]]; then
+    /notebooks/.gradient/automated-test.sh "${@:3}"
+fi
--- setup.sh
+++ setup.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
 
+echo "Graphcore setup - Starting notebook setup"
+DETECTED_NUMBER_OF_IPUS=$(python .gradient/available_ipus.py)
+if [[ "$1" == "test" ]]; then
+    IPU_ARG="${DETECTED_NUMBER_OF_IPUS}"
+else
+    IPU_ARG=${1:-"${DETECTED_NUMBER_OF_IPUS}"}
+fi
+echo "Graphcore setup - Detected ${DETECTED_NUMBER_OF_IPUS} IPUs"
+if [[ "${DETECTED_NUMBER_OF_IPUS}" == "0" ]]; then
+    echo "=============================================================================="
+    echo "                         ERROR  DETECTED"
+    echo "=============================================================================="
+    echo "Connection to IPUs timed-out. This error indicates a problem with the "
+    echo "hardware you are running on. Please contact Paperspace Support at "
+    echo " https://docs.paperspace.com/contact-support/ "
+    echo " referencing the Notebook ID: ${PAPERSPACE_METRIC_WORKLOAD_ID:-unknown}"
+    echo "=============================================================================="
+    exit -1
+fi
+
+export NUM_AVAILABLE_IPU=${IPU_ARG}
+export GRAPHCORE_POD_TYPE="pod${IPU_ARG}"
+
+export POPLAR_EXECUTABLE_CACHE_DIR="/tmp/exe_cache"
+export DATASETS_DIR="/tmp/dataset_cache"
+export CHECKPOINT_DIR="/tmp/checkpoints"
+
+# mounted public dataset directory (path in the container)
+# in the Paperspace environment this would be ="/datasets"
+export PUBLIC_DATASETS_DIR="/datasets"
+
+# Fine-tuning BERT uses the HF Squad dataset
+export HUGGINGFACE_HUB_CACHE="/tmp/huggingface_caches"
+export TRANSFORMERS_CACHE="/tmp/huggingface_caches/checkpoints"
+export HF_DATASETS_CACHE="/tmp/huggingface_caches/datasets"
+
+export POPTORCH_CACHE_DIR="${POPLAR_EXECUTABLE_CACHE_DIR}"
+export POPART_CACHE_DIR="${POPLAR_EXECUTABLE_CACHE_DIR}"
+export POPTORCH_LOG_LEVEL=ERR
+export RDMAV_FORK_SAFE=1
+
+
+echo "Graphcore setup - Spawning dataset preparation process"
+nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
+
+export PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp
+echo "Graphcore setup - Starting Jupyter kernel"
+jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
+            --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
+            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+
+
 #run python setup.py
 python setup.py
 #make sure pip has installed llama-inferrence
